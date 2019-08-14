84e00bd79805a31f8b8cb0dbd819545102de5b78
Ruben Ryckeghem <ruben.ryckeghem@student.ehb.be>
2019-08-14 18:51:41 +0200

    Bestelling view wijziging
    stijl aangepast, een min knop bijgevoegd en het tafelnummer weergegeven

--- resto/resources/views/content/bestelling.blade.php
+++ resto/resources/views/content/bestelling.blade.php
@@ -7,13 +7,17 @@
             <div class="col-lg-12" style="font-family: Steelfish;">
                 <h2>Bestelling</h2>
             </div>
+            <div class="col-lg-12" style="font-family: Steelfish;">
+                <h4>Tafelnummer: {{Session::get('tafelNummer')}}</h4>
+            </div>
             <div class="col-lg-12">
                 <ul class="list-group">
                     @foreach($products as $product)
                         <li class="list-group-item">
                             <span class="badge-danger" style="padding: 0.2em; border-radius: 10px;">{{$product['qty']}}</span>
-                            <strong>{{$product['item']['drankNaam']}}</strong>
+                            <strong>{{$product['item']['drankNaam']}}:</strong>
                             <span class="label label-succes">€{{$product['drankPrijs']}}</span>
+                            <button class="btn btn-outline-danger">-</button>
                         </li>
                     @endforeach
                 </ul>
@@ -22,6 +26,9 @@
         <div class="row" style="margin-top: 0.5em;">
             <div class="col-lg-12">
                 <strong>Totaal: €{{$totalPrice}}</strong>
+            </div>
+            <hr>
+            <div class="col-lg-12">
                 <button class="btn btn-outline-danger">Bestellen</button>
             </div>
         </div>
--- resto/resources/views/content/index.blade.php
+++ resto/resources/views/content/index.blade.php
@@ -19,7 +19,9 @@
             <img style="width: 100%;" src="{{ URL::asset("/images/history.png") }}">
         </div>
         <div class="col-4">
+            <a href="{{route('product.bestelling')}}">
             <img style="width: 100%;" src="{{ URL::asset("/images/bill.png") }}">
+            </a>
         </div>
         <div class="col-4" style="padding-left: 0; padding-right: 2em;">
            <a href="{{Route('contentStart')}}"><img style="width: 100%;" src="{{ URL::asset("/images/logOut.png") }}"></a>
