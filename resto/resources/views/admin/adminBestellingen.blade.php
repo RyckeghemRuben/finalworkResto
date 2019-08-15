77f9773cffcd379946d0910da78bf36c688f85db
Ruben Ryckeghem <ruben.ryckeghem@student.ehb.be>
2019-08-15 11:47:42 +0200

    Stijl bestellingen view aangepast

--- resto/resources/views/admin/adminBestellingen.blade.php
+++ resto/resources/views/admin/adminBestellingen.blade.php
@@ -9,11 +9,15 @@
              @foreach($bestellingen as $bestelling)
              <div class="card" style="margin-top: 1em;">
                  <div class="card-body">
-                     <h2 class="card-title" style="font-family: Steelfish">Tafelnummer: {{$bestelling->tafelnummer}}
-                     | <a href="{{route('bestellingDelete',['id'=>$bestelling->id])}}" class="btn btn-outline-danger">X</a>
-                     </h2>
-
-                     <hr>
+                     <div class="row">
+                         <div class="col-9">
+                         <h2 class="card-title" style="font-family: Steelfish">Tafelnummer: {{$bestelling->tafelnummer}}</h2>
+                         </div>
+                         <div class="col-3">
+                             <a href="{{route('bestellingDelete',['id'=>$bestelling->id])}}" class="btn btn-outline-danger">X</a>
+                         </div>
+                     </div>
+                     <hr style="border-color: #e3342f;">
                      @foreach($bestelling->cart->items as $item)
                             <p>
                                 <b>{{$item['item']['drankNaam']}}</b> | X <b>{{$item['qty']}}</b>
@@ -23,7 +27,7 @@
                          @endforeach
                  </div>
                  <div class="card-footer">
-                     <h5 class="card-title" style="margin: 0;">Totaalprijs: {{$bestelling->cart->totalPrice}}</h5>
+                     <h5 class="card-title" style="margin: 0;">Totaalprijs: € {{$bestelling->cart->totalPrice}}</h5>
                  </div>
              </div>
              @endforeach
